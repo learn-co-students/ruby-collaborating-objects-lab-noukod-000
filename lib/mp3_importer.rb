b8bdb5237653ef709f65dc5a06395aa0320d4fa7
Saint-Jean Germila <germila@noukod.com>
2018-10-11 16:13:21 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,21 @@
 class MP3Importer
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| song.new_by_filename(filename)}
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
     end
+    files
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
 end
